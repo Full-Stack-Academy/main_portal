3892e91d48a6223f7ab68d7c996a2a4dd9235da1
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-11-12 21:26:45 +0530

    SW and Manifest

--- vite.config.js
+++ vite.config.js
@@ -3,47 +3,47 @@ import postcss from "./postcss.config.js";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-const manifestForPlugIn = {
-  registerType: "prompt",
-  includeAssests: ["favicon.ico"],
-  manifest: {
-    name: "FSA Portal",
-    short_name: "fsa_portal",
-    description: "FSA Portal App",
-    icons: [
-      {
-        src: "/favicon_io/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/favicon_io/android-chrome-192x192.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/favicon_io/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/favicon_io/favicon.ico",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
-    ],
-    theme_color: "#171717",
-    background_color: "#f0e7db",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "portrait",
-  },
-};
+// const manifestForPlugIn = {
+//   registerType: "prompt",
+//   includeAssests: ["favicon.ico"],
+//   manifest: {
+//     name: "FSA Portal",
+//     short_name: "fsa_portal",
+//     description: "FSA Portal App",
+//     icons: [
+//       {
+//         src: "/favicon_io/android-chrome-192x192.png",
+//         sizes: "192x192",
+//         type: "image/png",
+//         purpose: "favicon",
+//       },
+//       {
+//         src: "/favicon_io/android-chrome-192x192.png",
+//         sizes: "512x512",
+//         type: "image/png",
+//         purpose: "favicon",
+//       },
+//       {
+//         src: "/favicon_io/apple-touch-icon.png",
+//         sizes: "180x180",
+//         type: "image/png",
+//         purpose: "apple touch icon",
+//       },
+//       {
+//         src: "/favicon_io/favicon.ico",
+//         sizes: "512x512",
+//         type: "image/png",
+//         purpose: "any maskable",
+//       },
+//     ],
+//     theme_color: "#171717",
+//     background_color: "#f0e7db",
+//     display: "standalone",
+//     scope: "/",
+//     start_url: "/",
+//     orientation: "portrait",
+//   },
+// };
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -53,7 +53,48 @@ export default defineConfig({
   css: {
     postcss,
   },
-  plugins: [react(), VitePWA({ manifest: manifestForPlugIn })],
+  plugins: [
+    react(),
+    // VitePWA({
+    //   manifest: {
+    //     name: "FSA Portal",
+    //     short_name: "fsa_portal",
+    //     description: "FSA Portal App",
+    //     icons: [
+    //       {
+    //         src: "/favicon_io/android-chrome-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "favicon",
+    //       },
+    //       {
+    //         src: "/favicon_io/android-chrome-192x192.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "favicon",
+    //       },
+    //       {
+    //         src: "/favicon_io/apple-touch-icon.png",
+    //         sizes: "180x180",
+    //         type: "image/png",
+    //         purpose: "apple touch icon",
+    //       },
+    //       {
+    //         src: "/favicon_io/favicon.ico",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //     ],
+    //     theme_color: "#171717",
+    //     background_color: "#f0e7db",
+    //     display: "standalone",
+    //     scope: "/",
+    //     start_url: "/",
+    //     orientation: "portrait",
+    //   },
+    // }),
+  ],
   resolve: {
     alias: [
       {
