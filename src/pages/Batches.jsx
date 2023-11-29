e230d6e234c16a997bb577673b2aa7b9ae0904f3
Syed Sayeeduddin <76648428+iamsayeeduddin@users.noreply.github.com>
2023-11-29 16:04:23 +0530

    Batches Page

---
+++ src/pages/Batches.jsx
@@ -0,0 +1,274 @@
+import WelcomeBanner from "../partials/dashboard/WelcomeBanner";
+import DashboardAvatars from "../partials/dashboard/DashboardAvatars";
+import FilterButton from "../components/DropdownFilter";
+import Datepicker from "../components/Datepicker";
+import AddModal from "../partials/placement/AddModal";
+import Banner from "../partials/Banner";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import EditModal from "../partials/placement/EditModal";
+
+const Batches = () => {
+  const [batches, setBatches] = useState([]);
+  const [addDis, setAddDis] = useState(false);
+  const [editDis, setEditDis] = useState(false);
+  const [testS, setTestS] = useState("");
+  const baseUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}api/batch/`)
+      .then((res) => {
+        setBatches(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [addDis, editDis]);
+
+  const openEdit = (dt) => {
+    setTestS(dt);
+    setEditDis(true);
+  };
+
+  return (
+    <>
+      {addDis && <AddModal dis={addDis} setDis={setAddDis} />}
+      {editDis && <EditModal dis={editDis} setDis={setEditDis} data={testS} />}
+      <main>
+        <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          {/* Dashboard actions */}
+          <div className="sm:flex sm:justify-between sm:items-center mb-8">
+            {/* Left: Avatars */}
+            {/* <DashboardAvatars /> */}
+            <h2 className="text-3xl font-semibold text-slate-800 dark:text-slate-100 mb-2">Batches</h2>
+
+            {/* Right: Actions */}
+            <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+              {/* Filter button */}
+              <FilterButton />
+              {/* Datepicker built with flatpickr */}
+              <Datepicker />
+              {/* Add view button */}
+              <button onClick={() => setAddDis(true)} className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
+                <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                  <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                </svg>
+                <span className="hidden xs:block ml-2">Add New</span>
+              </button>
+            </div>
+          </div>
+
+          {/* Cards */}
+          {/* <div classNameName="flex w-full"> */}
+          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="p-4">
+                    <div className="flex items-center">
+                      <input
+                        id="checkbox-all-search"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                      <label htmlFor="checkbox-all-search" className="sr-only">
+                        checkbox
+                      </label>
+                    </div>
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Course
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Branch
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Start Date
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Batch Time
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Mode
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Intake
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Edit
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Delete
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {batches &&
+                  batches.map((data, key) => (
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={key}>
+                      <td className="w-4 p-4">
+                        <div className="flex items-center">
+                          <input
+                            id="checkbox-table-search-1"
+                            type="checkbox"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                          />
+                          <label htmlFor="checkbox-table-search-1" className="sr-only">
+                            checkbox
+                          </label>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4">
+                        <img class="w-10 h-10 rounded-full" src={data.image} alt="Rounded avatar" />
+                      </td>
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {data.course.name}
+                      </th>
+                      <td className="px-6 py-4">{data.branch.name}</td>
+                      <td className="px-6 py-4">{data.startTime + " - " + data.endTime}</td>
+                      <td className="px-6 py-4">{data.mode}</td>
+                      <td className="px-6 py-4">
+                        <label class="relative inline-flex items-center mb-5 cursor-pointer">
+                          <input type="checkbox" checked={data?.isActive} class="sr-only peer" />
+                          <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        </label>
+                      </td>
+                      <td className="px-6 py-4">
+                        <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                          Edit
+                        </span>
+                      </td>
+                      <td className="px-6 py-4">
+                        <span className="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</span>
+                      </td>
+                    </tr>
+                  ))}
+                {!batches.length && (
+                  <tr className="justify-center bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </td>
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </th>
+                    <td className="px-6 py-4">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div role="status" class="max-w-sm animate-pulse">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+            <nav className="flex items-center justify-between pt-4" aria-label="Table navigation">
+              <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of{" "}
+                <span className="font-semibold text-gray-900 dark:text-white">{batches.length}</span>
+              </span>
+              <ul className="inline-flex -space-x-px text-sm h-8">
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Previous
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    1
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    2
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    aria-current="page"
+                    className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                  >
+                    3
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    4
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    5
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Next
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          {/* </div> */}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Batches;
